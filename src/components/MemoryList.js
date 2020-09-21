9a5a696cf9fb9fb20fb08411c5cf6be693960e55
Ben Russell <benjaminrussell36@gmail.com>
2020-09-21 11:33:42 -0700

    Add MemoryList component.
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: fetonecontrol <frederick.ernest@gmail.com>

---
+++ src/components/MemoryList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Memory from './Memory';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
+
+function MemoryList(props) {
+	useFirestoreConnect([{ collection: 'memories' }]);
+
+	const memories = useSelector((state) => state.firestore.ordered.memories);
+
+	if (isLoaded(memories)) {
+		return (
+			<React.Fragment>
+				<hr />
+				{memories.map((memory) => {
+					return (
+						<Memory
+							whenMemoryClicked={props.onMemorySelection}
+							title={memory.title}
+							date={memory.date}
+							description={memory.description}
+              place={memory.place}
+              vibe={memory.vibe}
+              scents={memory.scents}
+              keywords={memory.keywords}
+							id={memory.id}
+							key={memory.id}
+						/>
+					);
+				})}
+			</React.Fragment>
+		);
+	} else {
+		return (
+			<React.Fragment>
+				<h3>Loading...</h3>
+			</React.Fragment>
+		);
+	}
+}
+
+MemoryList.propTypes = {
+	onMemorySelection: PropTypes.func,
+};
+
+export default MemoryList;
