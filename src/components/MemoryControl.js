773a444ad6fe888e808c1bcd0e35c3ef59e33ece
Ben Russell <benjaminrussell36@gmail.com>
2020-09-21 14:10:27 -0700

    Add edit, delete methods
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: fetonecontrol <frederick.ernest@gmail.com>

--- src/components/MemoryControl.js
+++ src/components/MemoryControl.js
@@ -33,7 +33,44 @@ class MemoryControl extends React.Component {
     }
   }
 
-  
+  handleAddMemoryToList = () => {
+    this.setState({
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangingSelectedMemory = (id) => {
+    this.props.firestore.get({collection: 'memories', doc: id}).then((memory) => {
+      const firestoreMemory = {
+        title:  memory.get("title"),
+        date:  memory.get("date"),
+        description:  memory.get("desciption"),
+        place:  memory.get("place"),
+        vibe:  memory.get("vibe"),
+        scents:  memory.get("scents"),
+        keywords:  memory.get("keywords"),
+        id: id
+      }
+      this.setState({selectedMemory: firestoreMemory});
+    });
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  };
+
+  handleEditingMemoryInList = () => {
+    this.setState({
+      editing: false,
+      selectedMemory: null,
+    });
+  };
+
+  handleDeleteMemory = (id) => {
+    this.props.firestore.delete({collection: 'memories', doc: id});
+    this.setState({selectedMemory: null});
+  }
+
 }
 
 export default withFirestore(MemoryControl);
